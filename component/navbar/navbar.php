43ffc288acf869738b0f444c01ac29c7d1b0a52a
manu-lopez <manuellp87@gmail.com>
2019-11-12 14:44:04 +0100

    Añade cosas

---
+++ component/navbar/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include "../head/head.html" ?>
+<link rel="stylesheet" href="navbar.css">
+  <title>Document</title>
+</head>
+<body>
+<div class="nav-contact">
+<ul class="nav justify-content-around">
+  <li class="nav-item">
+  <button type="button" class="btn btn-outline-dark"><i class="fas fa-location-arrow"></i> Localización</button>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-dark disabled"><i class="fas fa-mobile-alt"> Teléfono 24h: +34 666 666 666</i></a>
+  </li>
+  <li class="d-flex nav-item svg1">
+    <img src="../../archivos_web/iconos/spain.svg">
+    <img src="../../archivos_web/iconos/united-kingdom.svg">
+  </li>
+</ul>
+</div>
+</body>
+</html>
