908f769d423ed02395933c8acd4ee99af17161bb
jab95 <josejab95@gmail.com>
2019-11-12 13:37:36 +0100

    añade footer web

---
+++ footer/footer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="footer.css">
+    <script src="https://kit.fontawesome.com/327a9188e5.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <footer class="page-footer font-small blue pt-4">
+        <div class="container-fluid text-center text-md-left">
+            <form>
+                <div class="row justify-content-center">
+                    <div class="form-group">
+                        <h5 class="text-uppercase">Suscíbete a nuestro newsletter</h5>
+                        <div class="btnNewsletter">
+                            <input type="text" class="form-control" id="nombre" name="nombre">
+                            <button type="submit" class="btn btn-success"> Enviar </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="checkbox1" name="checkbox1">
+                            <label class="form-check-label" for="checkbox1">
+                                He leido y acepto la política de privacidad y los términos de uso de la web
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-around">
+                    <div class="col-md-4 ">
+                        <h5 class="text-uppercase">Contacto:</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <i class="fas fa-mobile-alt"> +34 666 666 666</i>
+                            </li>
+                            <li>
+                            <i class="far fa-envelope"> contacto@mishuellitas.com</i>
+                            </li>
+                            <li>
+                                <a href="#!"><i class="fab fa-facebook-f  fa-3x"></i></a>
+                                <a href="#!"><i class="fab fa-instagram  fa-3x"></i></i></a>
+                                <a href="#!"><i class="fab fa-twitter  fa-3x"></i></i></a>
+                            </li>
+                           
+                        </ul>
+
+                    </div>
+                    <div class="col-md-2 ">
+
+                        <h5 class="text-uppercase">Nuestros servícios</h5>
+
+                        <ul class="list-unstyled enlaces">
+                            <li>
+                                <a href="#!">Veterinaria</a>
+                            </li>
+                            <li>
+                                <a href="#!">Peluquería</a>
+                            </li>
+                            <li>
+                                <a href="#!">Tienda Online</a>
+                            </li>
+                            <li>
+                                <a href="#!">Adiestramiento</a>
+                            </li>
+                            <li>
+                                <a href="#!">Guardería</a>
+                            </li>
+                            <li>
+                            <li>
+                                <a href="#!">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#!">Contacto</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+            </form>
+
+
+            <div class="footer-copyright text-center py-3 footer">© 2019 Copyright:
+                <a href="https://github.com/jab95">Pepe</a> y <a href="https://github.com/manu-lopez">Manuel</a>
+            </div>
+
+    </footer>
+    <!-- Footer -->
+</body>
+
+</html>
