b4491d74a74f111e2867b5dec6fb3749d6afaee1
jab95 <josejab95@gmail.com>
2019-11-13 10:17:40 +0100

    añade formulario

---
+++ component/formulario/formulario.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../head/head.html' ?>
+</head>
+
+<body>
+<form>
+    <div class="container-fluid text-center text-md-left">
+            <div class="card col-lg-7">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4 ">
+                            <div class="form-group">
+                                <h3>Nombre</h3>
+                                <input type="text" class="form-control" id="nombre" name="nombre">
+                            </div>
+                            <div class="form-group">
+                                <h3 for="nombre">Email</h3>
+                                <input type="email" class="form-control" id="email" name="email">
+                            </div>
+                        </div>
+                        <div class="col-md-8 ">
+                            <div class="form-group">
+                                <h3 for="comentarios">Mensaje</h3>
+                                <textarea class="form-control" rows="5" id="comentarios" name="comentarios"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 ">
+                            <p class="card-text">Le informamos que sus datos personales serán tratados por MISHUELLITAS,
+                                con el fin de gestionar su solicitud y el mantenimiento de las relaciones profesionales
+                                y comerciales con usted. Sus datos no serán cedidos a terceros. Esta tratamiento de
+                                datos es necesario para atender su solicitud. Sus datos serán conservados hasta que
+                                Usted se dé de baja del servicio y/o pasado un tiempo prudencial desde que atendamos su
+                                solicitud.
+                                Ud. puede ejercer sus derechos de acceso, rectificación, cancelación, oposición,
+                                portabilidad y limitación del tratamiento de sus datos dirigiéndose a MISHUELLITAS IES
+                                Francisco romero vargas o a contacto@mishuellitas.com, acompañando copia de su DNI
+                                acreditando debidamente su identidad.
+                                En cualquier situación, Ud. tiene derecho a presentar una reclamación ante la Agencia
+                                Española de Protección de Datos (AEPD). </p>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="checkbox1"
+                                    name="checkbox1">
+                                <label class="form-check-label" for="checkbox1">
+                                    Acepto ley de protección de datos
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                    <div class="col-md-12 ">
+                    <button type="submit" class="btn-outline-dark btn-formulario">Enviar</button>   
+                    </div>    
+                </div>
+                </div>
+            </div>
+    </div>
+</form>
+</body>
+
+</html>
