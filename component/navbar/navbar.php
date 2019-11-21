b1ce858c35bd8ab366425b55f4da24ce49797154
manu-lopez <manuellp87@gmail.com>
2019-11-21 10:17:27 +0100

    Añade componente Navbar, modifica prototipo y añade logo blanco

--- component/navbar/navbar.php
+++ component/navbar/navbar.php
@@ -6,34 +6,75 @@
   <title>Document</title>
 </head>
 <body>
-<div class="nav-contact">
-<ul class="nav justify-content-around">
-  <li class="nav-item btn-location-phone">
-  <button type="button" class="btn btn-outline-dark"><i class="fas fa-location-arrow"></i> Localización</button>
-  </li>
-  <li class="nav-item btn-location-phone">
-    <a class="nav-link text-dark disabled"><i class="fas fa-mobile-alt"> Teléfono 24h: +34 666 666 666</i></a>
-  </li>
-  <li class="d-flex nav-item svg">
-    <img src="../../archivos_web/iconos/spain.svg">
-    <img src="../../archivos_web/iconos/united-kingdom.svg">
-  </li>
-</ul>
+  <!-- Navbar emergencias -->
+<div class="container-fluid">
+  <div class="nav-contact">
+    <ul class="nav justify-content-around">
+      <li class="nav-item btn-location-phone">
+      <button type="button" class="btn btn-outline-dark"><i class="fas fa-location-arrow"></i> Localización</button>
+      </li>
+      <li class="nav-item btn-location-phone">
+        <a class="nav-link text-dark disabled"><i class="fas fa-mobile-alt"> Teléfono 24h: +34 666 666 666</i></a>
+      </li>
+      <li class="d-flex nav-item">
+        <a class="svg" href="#">
+          <img src="../../archivos_web/iconos/spain.svg">
+        </a>
+        <a class="svg" href="#">
+          <img src="../../archivos_web/iconos/united-kingdom.svg">
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- Navbar Principal -->
+  <nav class="d-flex justify-content-around nav-principal navbar navbar-expand-lg navbar-dark">
+  <div class="contenedorlogo ">
+      <a class="logo" href="#">
+        <img src="../../archivos_web/logos/logo_blanco.png" width="120" height="70" alt="">
+      </a>
+  </div>
+    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Tienda</a>
+          
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Citas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Veterinaria</a>
+          <a class="dropdown-item" href="#">Guarderia</a>
+          <a class="dropdown-item" href="#">Peluqueria</a>
+          <a class="dropdown-item" href="#">Adiestramiento</a>
+        </div>
+      </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Adopción</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Contacto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-caret-down"></i> Acceder</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
 </div>
-<nav class="nav-principal">
-  <a class="logo" href="#">
-    <img src="../../archivos_web/logos/logo_blanco.svg" width="120" height="70" alt="">
-  </a>
-  <ul class="list-navbar">
-    <i class="fas fa-caret-down"></i><a href="">Servicios</a>
-    <i class="fas fa-caret-down"></i><a href="">Tienda</a>
-    <i class="fas fa-caret-down"></i><a href="">Citas</a>
-    <i class="fas fa-caret-down"></i><a href="">Adopción</a>
-    <i class="fas fa-caret-down"></i><a href="">Blog</a>
-    <i class="fas fa-caret-down"></i><a href="">Nosotros</a>
-    <i class="fas fa-caret-down"></i><a href="">Contacto</a>
-    <i class="fas fa-caret-down"></i><a href="">Acceder</a>
-  </ul>
-</nav>
 </body>
 </html>
