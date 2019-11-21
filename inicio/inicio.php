200b1c9fc8bc3e6719d1ec904c7fb020ed096a0e
jab95 <josejab95@gmail.com>
2019-11-21 13:48:24 +0100

    separa formulario aparte de inicio

--- inicio/inicio.php
+++ inicio/inicio.php
@@ -58,9 +58,7 @@
     </center>
 
     <div class="row  justify-content-center mt-5 fila-inicio">
-
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent ">
           <div class="card-body ">
 
@@ -72,11 +70,9 @@
 
           </div>
         </div>
-
       </div>
 
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent ">
           <div class="card-body ">
 
@@ -88,16 +84,11 @@
 
           </div>
         </div>
-
       </div>
-
-
     </div>
 
     <div class="row justify-content-center   fila-inicio">
-
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent ">
           <div class="card-body ">
 
@@ -108,11 +99,9 @@
 
           </div>
         </div>
-
       </div>
 
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent ">
           <div class="card-body ">
 
@@ -124,18 +113,12 @@
 
           </div>
         </div>
-
       </div>
-
-
-
     </div>
 
 
     <div class="row justify-content-center  fila-inicio">
-
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent ">
           <div class="card-body ">
 
@@ -147,11 +130,9 @@
 
           </div>
         </div>
-
       </div>
 
       <div class="col-lg-3 col-inicio">
-
         <div class="card text-center bg-transparent e">
           <div class="card-body ">
 
@@ -163,82 +144,23 @@
 
           </div>
         </div>
-
       </div>
-
     </div>
 
     <img src="../imagenes/linea.svg" id="linea1" alt="">
 
 
     <div class="donde-estamos">
-      <form>
-        <div class="container-fluid text-center text-md-left">
-          <div class="card col-lg-6">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-md-4 ">
-                  <div class="form-group">
-                    <h3>Nombre</h3>
-                    <input type="text" class="form-control" id="nombre" name="nombre">
-                  </div>
-                  <div class="form-group">
-                    <h3 for="nombre">Email</h3>
-                    <input type="email" class="form-control" id="email" name="email">
-                  </div>
-                </div>
-                <div class="col-md-8 ">
-                  <div class="form-group">
-                    <h3 for="comentarios">Mensaje</h3>
-                    <textarea class="form-control" rows="5" id="comentarios" name="comentarios"></textarea>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-12 ">
-                  <p class="card-text">Le informamos que sus datos personales serán tratados por MISHUELLITAS,
-                    con el fin de gestionar su solicitud y el mantenimiento de las relaciones profesionales
-                    y comerciales con usted. Sus datos no serán cedidos a terceros. Esta tratamiento de
-                    datos es necesario para atender su solicitud. Sus datos serán conservados hasta que
-                    Usted se dé de baja del servicio y/o pasado un tiempo prudencial desde que atendamos su
-                    solicitud.
-                    Ud. puede ejercer sus derechos de acceso, rectificación, cancelación, oposición,
-                    portabilidad y limitación del tratamiento de sus datos dirigiéndose a MISHUELLITAS IES
-                    Francisco romero vargas o a contacto@mishuellitas.com, acompañando copia de su DNI
-                    acreditando debidamente su identidad.
-                    En cualquier situación, Ud. tiene derecho a presentar una reclamación ante la Agencia
-                    Española de Protección de Datos (AEPD). </p>
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkbox1" name="checkbox1">
-                    <label class="form-check-label" for="checkbox1">
-                      Acepto ley de protección de datos
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div class="row mt-4">
-                <div class="col-md-12 ">
-                  <button type="submit" class="btn-outline-dark btn-formulario">Enviar</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>
-
-
-
+      <?php include '../component/formulario/form.php'; ?>
     </div>
 
-    
     <img src="../imagenes/linea.svg" id="linea2" alt="">
 
-
-
     <div class="footer-inicio">
-      <?php include '../component/footer/footer.php'; ?>
-    </div>
 
+      <?php include '../component/footer/footer.php'; ?>
+
+    </div>
   </div>
 
 
