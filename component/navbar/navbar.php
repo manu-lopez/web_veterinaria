c4fac9ddec0c32316a96a5b4922d5fd064274cd6
manu-lopez <manuellp87@gmail.com>
2019-11-22 23:45:39 +0100

    Arregla flecha dropdown citas

--- component/navbar/navbar.php
+++ component/navbar/navbar.php
@@ -38,7 +38,7 @@
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Citas
+            <i class="fas fa-caret-down"></i> Citas
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Veterinaria</a>
