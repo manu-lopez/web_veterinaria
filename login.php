1025b79f695bf8c040603cf0cb6e06fe679da29f
jab95 <josejab95@gmail.com>
2019-11-29 14:49:35 +0100

    añade pagina login

---
+++ login.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'component/head.html'; ?>
+</head>
+
+<body>
+
+
+    <?php include 'component/navbar.php'; ?>
+
+    <div class="body-login">
+
+        <div class="container-fluid">
+
+            <center>
+                <h2 class="text-white ">ACCEDER</h2>
+            </center>
+
+
+
+            <div class="row justify-content-center contacto mt-5 ml-4 mr-4">
+                <div class="col-lg-6">
+                    <form>
+                        <div class="container-fluid text-center text-md-left ">
+                            <div class="card col-lg">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-4 ">
+                                            <div class="form-group">
+                                                <h3>Nombre</h3>
+                                                <input type="text" class="form-control" id="nombre" name="nombre">
+                                            </div>
+                                            <div class="form-group">
+                                                <h3 for="nombre">Email</h3>
+                                                <input type="email" class="form-control" id="email" name="email">
+                                            </div>
+                                            <div class="form-group">
+                                                <h3 for="nombre">Contraseña</h3>
+                                                <input type="email" class="form-control" id="email" name="email">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 ">
+                                            <div class="form-group">
+                                                <h3>Apellidos</h3>
+                                                <input type="text" class="form-control" id="nombre" name="nombre">
+                                            </div>
+                                            <div class="form-group">
+                                                <h3 for="nombre">Repetir email</h3>
+                                                <input type="email" class="form-control" id="email" name="email">
+                                            </div>
+                                            <div class="form-group">
+                                                <h3 for="nombre">Repetir contraseña</h3>
+                                                <input type="email" class="form-control" id="email" name="email">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="checkbox1" name="checkbox1">
+                                                <label class="form-check-label" for="checkbox1">
+                                                    He leido y acepto la <a class="enlace-estilo" href="">política de privacidad</a> y los <a class="enlace-estilo" href=""> términos de uso
+                                                        de la web</a>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md">
+                                            <center>
+                                                <button type="submit" class="btn-outline-dark btn-formulario mt-5">Enviar</button>
+                                            </center>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="card col-lg-4">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md ">
+                                <div class="form-group">
+                                    <h3>Email</h3>
+                                    <input type="text" class="form-control" id="nombre" name="nombre">
+                                </div>
+                                <div class="form-group">
+                                    <h3 for="nombre">Contraseña</h3>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <center>
+                                    <button type="submit" class="btn-outline-dark btn-formulario mt-5">Enviar</button>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+            <div class="footer-inicio">
+
+                <?php include 'component/footer.php'; ?>
+
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
