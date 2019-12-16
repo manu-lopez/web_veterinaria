92b2b4c70b8616dcb9df501da08c34d2883e2031
jab95 <josejab95@gmail.com>
2019-12-16 13:22:14 +0100

    añade cambio flex footer

--- component/footer.php
+++ component/footer.php
@@ -1,88 +1,98 @@
 <img src="imagenes/linea.svg" class="linea2" alt="">
 
-<footer class="page-footer font-small blue pt-4">
-    <div class="container-fluid text-center text-md-left">
-        <form>
-            <div class="row justify-content-center filafooter">
+<footer>
+    <div class="body-footer">
 
-                <div class="col-md-3">
+        <div class="contenido-footer filafooter">
 
-                    <h5 class="text-uppercase">Nuestros servícios</h5>
+            <div class="column-servicios">
 
-                    <ul class="list-unstyled enlaces">
-                        <li>
-                            <a href="#!">Veterinaria</a>
-                        </li>
-                        <li>
-                            <a href="#!">Peluquería</a>
-                        </li>
-                        <li>
-                            <a href="#!">Tienda Online</a>
-                        </li>
-                        <li>
-                            <a href="#!">Adiestramiento</a>
-                        </li>
-                        <li>
-                            <a href="#!">Guardería</a>
-                        </li>
-                        <li>
-                        <li>
-                            <a href="#!">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#!">Contacto</a>
-                        </li>
-                    </ul>
+                <h5 class="text-uppercase">Nuestros servícios</h5>
 
-                </div>
+                <ul class="list-unstyled enlaces">
+                    <li>
+                        <a href="construccion.php">Veterinaria</a>
+                    </li>
+                    <li>
+                        <a href="construccion.php">Peluquería</a>
+                    </li>
+                    <li>
+                        <a href="construccion.php">Tienda Online</a>
+                    </li>
+                    <li>
+                        <a href="construccion.php">Adiestramiento</a>
+                    </li>
+                    <li>
+                        <a href="construccion.php">Guardería</a>
+                    </li>
+                    <li>
+                    <li>
+                        <a href="blog.php">Blog</a>
+                    </li>
+                    <li>
+                        <a href="contacto.php">Contacto</a>
+                    </li>
+                </ul>
 
-                <div class="col-md-3 ">
+            </div>
 
-                    <h5 class="text-uppercase">Contacto:</h5>
+            <div class="column-contacto">
 
-                    <ul class="list-unstyled">
-                        <li>
-                            <i class="fas fa-mobile-alt"> +34 666 666 666</i>
-                        </li>
-                        <li>
-                            <i class="far fa-envelope"> contacto@mishuellitas.com</i>
-                        </li>
-                        <li id="liSocial">
-                            <a href="http://facebook.com">
-                                <p class="bg-fb"></p>
-                            </a>
-                            <a href="http://instagram.com">
-                                <p class="bg-instagram"></p>
-                            </a>
-                            <a href="http://twitter.com">
-                                <p class="bg-twitter"></p>
-                            </a>
-                        </li>
-                    </ul>
 
-                </div>
+                <h5 class="text-uppercase">Contacto:</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <i class="fas fa-mobile-alt"> +34 666 666 666</i>
+                    </li>
+                    <li>
+                        <i class="far fa-envelope"> contacto@mishuellitas.com</i>
+                    </li>
+                    <li id="liSocial">
+                        <a href="http://facebook.com">
+                            <p class="bg-fb"></p>
+                        </a>
+                        <a href="http://instagram.com">
+                            <p class="bg-instagram"></p>
+                        </a>
+                        <a href="http://twitter.com">
+                            <p class="bg-twitter"></p>
+                        </a>
+                    </li>
+                </ul>
 
-                <div class="col-md-4 ml-5">
-                    <div class="form-group">
+            </div>
+
+            <div class="column-newsletter">
+
+                <div class="form-group">
+                    <center>
                         <h5 class="text-uppercase">Suscríbete a nuestro newsletter</h5>
-                        <div class="btn-newsletter">
-                            <input type="text" class="form-control input-footer" id="nombre" name="nombre" placeholder="Indique su email de subscripción">
-                            <button type="submit" class="btn btn-color"> Enviar </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="checkbox1" name="checkbox1">
-                            <label class="form-check-label" for="checkbox1">
-                                He leído y acepto la <a class="enlace-estilo" href="">política de privacidad</a> y los <a class="enlace-estilo" href=""> términos de uso
-                                    de la web</a>
-                            </label>
-                        </div>
+                    </center>
+                    <div class="btn-newsletter">
+                        <input type="text" class="form-control input-footer" id="nombre" name="nombre" placeholder="Indique su email de subscripción">
+                        <button type="submit" class="btn btn-color"> Enviar </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox1" name="checkbox1">
+                        <label class="form-check-label" for="checkbox1">
+                            He leído y acepto la <a class="enlace-estilo" href="">política de privacidad</a> y los <a class="enlace-estilo" href=""> términos de uso
+                                de la web</a>
+                        </label>
                     </div>
                 </div>
-            </div>
-        </form>
-    </div>
 
-    <div class="footer-copyright text-center py-3 footer">© 2019 Copyright:
-        <a href="https://github.com/jab95" class="enlace-estilo">Pepe</a> y <a href="https://github.com/manu-lopez" class="enlace-estilo">Manuel</a>
+            </div>
+
+
+        </div>
+
+        <center>
+            <div class="copyright">
+                © 2019 Copyright:
+                <a href="https://github.com/jab95" class="enlace-estilo">Pepe</a> y <a href="https://github.com/manu-lopez" class="enlace-estilo">Manuel</a>
+
+            </div>
+        </center>
+
     </div>
 
 </footer>
