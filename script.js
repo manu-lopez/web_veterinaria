636999541fda1dff8b08fd2cd7e4bd764ec6c1e5
Manu <manuellp87@gmail.com>
2020-02-06 21:23:58 +0100

    Corrige rutas

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // Redirecciona a Inicio
 Mousetrap.bind('ctrl+1', function () {
-  window.location.href = "http://localhost:8080/index.php";
+  window.location.href = "/index.php";
 });
 
 // Redirecciona a Tienda
 Mousetrap.bind('ctrl+2', function () {
-  window.location.href = "http://localhost:8080/tienda.php";
+  window.location.href = "/tienda.php";
 });
 
 // Redirecciona a Citas
 Mousetrap.bind('ctrl+3', function () {
-  window.location.href = "http://localhost:8080/citas.php";
+  window.location.href = "/citas.php";
 });
 
 // Redirecciona a Adopción
 Mousetrap.bind('ctrl+4', function () {
-  window.location.href = "http://localhost:8080/adopcion.php";
+  window.location.href = "/adopcion.php";
 });
 
 // Redirecciona a Blog
 Mousetrap.bind('ctrl+5', function () {
-  window.location.href = "http://localhost:8080/blog.php";
+  window.location.href = "/blog.php";
 });
 
 // Redirecciona a Nosotros
 Mousetrap.bind('ctrl+6', function () {
-  window.location.href = "http://localhost:8080/nosotros.php";
+  window.location.href = "/nosotros.php";
 });
 
 // Redirecciona a Contacto
 Mousetrap.bind('ctrl+7', function () {
-  window.location.href = "http://localhost:8080/contacto.php";
+  window.location.href = "/contacto.php";
 });
 
 // Redirecciona a Acceder
 Mousetrap.bind('ctrl+8', function () {
-  window.location.href = "http://localhost:8080/login.php";
+  window.location.href = "/login.php";
 });
 
 
