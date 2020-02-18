60723716e5706bb6be83fd8049fbb4a9df7c784d
Manu <manuellp87@gmail.com>
2020-02-18 13:25:59 +0100

    Corrige errores responsive

--- tienda.php
+++ tienda.php
@@ -12,47 +12,54 @@
 
         <div class="container-fluid">
             <div class="row justify-content-center contacto mt-5 ml-3 mr-3">
-
                 <div class="col-lg-12">
                     <form>
                         <div class="container-fluid text-start text-md-left entradas">
                             <div class="card col-lg card-entrada">
                                     <h2 class="text-white pt-3">Artículos en oferta</h2>
+
                                 <div class="card-body">
                                     <div class="row">
+
                                         <div class="col-lg ">
                                             <img class="d-block w-100 img-tienda" src="imagenes/comida.png" alt="">
-                                                <p id="producto1">Producto 1 </p>
-                                                <p id="precio1">9.99</p>
-                                                <button id="1" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
+                                            <p id="producto1">Producto 1 </p>
+                                            <p id="precio1">9.99</p>
+                                            <button id="1" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
                                         </div>
+
                                         <div class="col-lg ">
                                             <img class="d-block w-100 img-tienda" src="imagenes/comida.png" alt="">
-                                                <p id="producto2">Producto 2 </p>
-                                                <p id="precio2">3.99</p>
-                                                <button id="2" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
+                                            <p id="producto2">Producto 2 </p>
+                                            <p id="precio2">3.99</p>
+                                            <button id="2" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
                                         </div>
+
                                         <div class="col-lg ">
                                             <img class="d-block w-100 img-tienda" src="imagenes/comida.png" alt="">
-                                                <p id="producto3">Producto 3 </p>
-                                                <p id="precio3">5.99</p>
-                                                <button id="3" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
+                                            <p id="producto3">Producto 3 </p>
+                                            <p id="precio3">5.99</p>
+                                            <button id="3" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
                                         </div>
+
                                         <div class="col-lg ">
                                             <img class="d-block w-100 img-tienda" src="imagenes/comida.png" alt="">
-                                                <p id="producto4">Producto 4 </p>
-                                                <p id="precio4">2.99</p>
-                                                <button id="4" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
+                                            <p id="producto4">Producto 4 </p>
+                                            <p id="precio4">2.99</p>
+                                            <button id="4" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
                                         </div>
+
                                         <div class="col-lg ">
                                             <img class="d-block w-100 img-tienda" src="imagenes/comida.png" alt="">
-                                                <p id="producto5">Producto 5 </p>
-                                                <p id="precio5">1.99</p>
-                                                <button id="5" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
+                                            <p id="producto5">Producto 5 </p>
+                                            <p id="precio5">1.99</p>
+                                            <button id="5" type="button" class="btn-outline-dark btn-cesta">Añadir a cesta</button>
                                         </div>
+
                                     </div>
                                 </div>
                             </div>
+                            
                             <div class="card col-lg card-entrada">
                                     <h2 class="text-white pt-3">Artículos más vendidos</h2>
                                 <div class="card-body">
